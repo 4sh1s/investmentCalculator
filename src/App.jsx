f65769c3dc4b00fdb02e1b085eec40ae109e300a
4sh1s <tripathy.ashish251997@gmail.com>
2024-05-28 23:20:05 +0530

    finished investment calculator app -- Huh this was Short!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import { useState } from "react";
+import Results from "./components/Results";
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 15,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+  const inputIsValid = userInput.duration>=1;
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header></Header>
+      <UserInput userInput={userInput} onChange={handleChange}></UserInput>
+      {!inputIsValid && <p className="center">Please Enter a valid non negative duration value</p>}
+      {inputIsValid &&<Results userInput={userInput}></Results>}
+    </>
+  );
 }
 
-export default App
+export default App;
